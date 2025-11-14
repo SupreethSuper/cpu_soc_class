ec06971d5fc43da1b801a6659506d2ce5a5bea58
SupreethSuper <supreeth.sandri@gmail.com>
2025-11-13 18:21:02 -0700

    cam cache test - 1

--- cam.sv
+++ cam.sv
@@ -9,21 +9,27 @@ module cam
    parameter TAG_SZ=8                   // size of the tag
    )
    (
-
+//---------------------outputs only----------------------------------------------
    output logic [BITS-1:0]    data,        // the data
    output logic               found_it,    // was in the CAM
+//----------------------end of outputs--------------------------------------------
 
-   input  [TAG_SZ-1:0]        check_tag,   // the tag to match
+//------------------------inputs only-------------------------------------------
+
+//----------------------------control signals-------------------------------------
    input                      read,        // read signal
-
+   input                      clk,         // system clock
    input                      write_,      // write_ signal
+   input                      rst_         // system reset
+   input                      new_valid,   // new valid bit
+//---------------------------------------------------------------------------------
+
+//-----------------------------------data------------------------------------------
+   input  [TAG_SZ-1:0]        check_tag,   // the tag to match
    input  [ADDR_LEFT:0]       w_addr,      // address to write
    input  [BITS-1:0]          wdata,       // data to write
    input  [TAG_SZ-1:0]        new_tag,     // the new tag
-   input                      new_valid,   // new valid bit
-
-   input                      clk,         // system clock
-   input                      rst_         // system reset
+//--------------------------------------------------------------------------------------
    );
 
    `include "cam_params.vh"
@@ -36,7 +42,53 @@ module cam
    logic [ADDR_LEFT:0] match_index;     // where we found it
    logic found;                         // did we find it
 
-   assign data = { BITS { 1'b0 } };
-   assign found_it = 1'b0;
+   //the reset block
+   always_ff @( posedge clk or negedge rst_ ) begin //reset
+    
+    if(!rst_) begin
+      //all the mems set to 0
+      for(index < 0; index < WORDS; index = index + 1) begin
+        data_mem[ index ] <= { BITS { 1'b0 } };
+        tag_mem [ index ] <= { BITS { 1'b0 } };
+        val_mem [ index ] <= { BITS { 1'b0 } };
+      end
+
+      else begin
+        //now checking if write_ == 0, cause condition executing at !write_, which means that !write_ =1, which means that write_ = 0
+        if(write_)
+
+          data_mem[ w_addr ] <= wdata;
+          tag_mem [ w_addr ] <= new_tag;
+          val_mem [ w_addr ] <= new_valid;
+      end
+      //if not the write_, then its the read, and using if, cause not taking the risk of using if else
+      // if(read) begin
+        
+
+
+
+      // end
+    end
+
+
+   end
+
+//I guess this is equlivalent to if(read)
+always @(read) begin
+    found       = 1'b0;
+    match_index = INDEX[0];
+
+    for (index = 0; index < WORDS; index = index + 1) begin
+        if (val_mem[index] && (tag_mem[index] == input_tag)) begin
+            match_index = INDEX[index];
+            found       = 1'b1;
+        end
+    end
+end
+
+
+
+   assign data = found ? data_mem[match_index] : { BITS { 1'b0 } };
+   assign found_it = found;
 
 endmodule
